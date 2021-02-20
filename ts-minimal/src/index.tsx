eea5bb6c2781a002dc6fb27dd7f7158290beade8
Alexandre <amoutonbrady@gmail.com>
2021-02-20 16:46:23 +0100

    :arrow_up: Update depdencies (HMR :tada:)

--- ts-minimal/src/index.tsx
+++ ts-minimal/src/index.tsx
@@ -2,9 +2,5 @@ import { render } from "solid-js/web";
 
 import App from "./App";
 
-const dispose = render(() => <App />, document.getElementById("root"));
+render(() => <App />, document.getElementById("root"));
 
-if (import.meta.hot) {
-  import.meta.hot.accept();
-  import.meta.hot.dispose(dispose);
-}
--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -3,9 +3,4 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 
-const dispose = render(() => <App />, document.getElementById("root"));
-
-if (import.meta.hot) {
-  import.meta.hot.accept();
-  import.meta.hot.dispose(dispose);
-}
+render(() => <App />, document.getElementById("root"));
