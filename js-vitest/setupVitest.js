21dc20ea456003ba0c06ba59ba16f8c68476c5b2
michaelspiss <michaelspiss.dev@mailbox.org>
2022-06-02 00:27:47 +0200

    Swap postinstall for simple imports

--- js-vitest/setupVitest.js
+++ js-vitest/setupVitest.js
@@ -1,1 +1,4 @@
-import '@testing-library/jest-dom';
+import {expect} from "vitest";
+import matchers from "@testing-library/jest-dom/dist/matchers";
+
+expect.extend(matchers)
--- ts-vitest/setupVitest.ts
+++ ts-vitest/setupVitest.ts
@@ -1,1 +1,13 @@
-import '@testing-library/jest-dom';
+import {expect} from "vitest";
+// @ts-ignore
+import matchers from "@testing-library/jest-dom/dist/matchers";
+import {TestingLibraryMatchers} from "@testing-library/jest-dom/matchers"
+
+declare global {
+    namespace Vi {
+        interface Assertion extends TestingLibraryMatchers<any, void> {}
+        interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
+    }
+}
+
+expect.extend(matchers)
