43c04746046aa0db563229c68f730e5822988db5
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-31 10:55:54 +0200

    update vitest & remove solid-js from inlined deps

--- ts-vitest/vite.config.ts
+++ ts-vitest/vite.config.ts
@@ -16,11 +16,6 @@ export default defineConfig({
       web: [/\.[jt]sx?$/],
     },
     setupFiles: './setupVitest.ts',
-    // solid needs to be inline to work around
-    // a resolution issue in vitest:
-    deps: {
-      inline: [/solid-js/],
-    },
     // if you have few tests, try commenting one
     // or both out to improve performance:
     threads: false,
