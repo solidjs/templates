4a5a36e5f0a0ac3fa2f2d7ba04029e8a29a027a4
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-01-27 16:25:32 +0000

    Update index.tsx

--- ts-tauri/src/index.tsx
+++ ts-tauri/src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 
-import './index.css'
+import '@styles/imports.css'
 import App from './App'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
