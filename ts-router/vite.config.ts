809e406c7915b7a641f6b89410d6b81d715224fc
Birk Skyum <birk.skyum@pm.me>
2024-11-27 00:20:24 +0100

    remove outdated devtools

--- ts-router/vite.config.ts
+++ ts-router/vite.config.ts
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
