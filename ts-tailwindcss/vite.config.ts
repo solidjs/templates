2419bc8f9d66b7730231abf384cd52d286fe7330
Bobae Kang <hello@bobaekang.com>
2025-03-02 11:17:17 -0600

    Migrate tailwindcss setup to vite (#160)

--- ts-tailwindcss/vite.config.ts
+++ ts-tailwindcss/vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
   },
