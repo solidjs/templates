499e61df0cd91e5a2766e39aab2e092f00cfea16
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2025-09-19 23:10:47 +0200

    Remove js and ts root repos, since they are in /vanilla (#216)

--- ts-sass/src/Counter.tsx
+++
@@ -1,3 +0,0 @@
-export default function Counter() {
-  return <h1>Hello world</h1>;
-}
--- ts-unocss/unocss.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@unocss/vite';
-import { presetMini } from '@unocss/preset-mini';
-
-export default defineConfig({
-  presets: [presetMini()],
-});
