46850999c883b37858ebd4eb54d83eeb24a689d6
Birk Skyum <birk.skyum@pm.me>
2025-11-10 23:57:04 +0100

    update to vite env tss

--- tanstack-start/app/components/CustomMessage.tsx
+++
@@ -1,8 +0,0 @@
-export function CustomMessage({ message }: { message: string }) {
-  return (
-    <div class="py-2">
-      <div class="italic">This is a custom message:</div>
-      <p>{message}</p>
-    </div>
-  );
-}
