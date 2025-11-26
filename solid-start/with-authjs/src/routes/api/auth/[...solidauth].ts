da88d0713cc9832c8f14b7febecc39ca6df25463
Birk Skyum <birk.skyum@pm.me>
2025-11-26 21:19:11 +0100

    use AuthRequestContext

--- solid-start/with-authjs/src/routes/api/auth/[...solidauth].ts
+++ solid-start/with-authjs/src/routes/api/auth/[...solidauth].ts
@@ -1,13 +1,13 @@
-import type { APIEvent } from "@solidjs/start/server";
+import type { AuthRequestContext } from "start-authjs";
 import { StartAuthJS } from "start-authjs";
 import { authConfig } from "~/server/auth";
 
 const { GET: AuthGET, POST: AuthPOST } = StartAuthJS(authConfig);
 
-export const GET = (event: APIEvent) => {
+export const GET = (event: AuthRequestContext) => {
   return AuthGET({ request: event.request, response: new Response() });
 };
 
-export const POST = (event: APIEvent) => {
+export const POST = (event: AuthRequestContext) => {
   return AuthPOST({ request: event.request, response: new Response() });
 };
