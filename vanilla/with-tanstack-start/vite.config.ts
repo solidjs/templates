ff9c0c60e6c4bbc4db304697fd7ed6dd8b23a724
Birk Skyum <birk.skyum@pm.me>
2025-11-27 01:53:40 +0100

    add nitro v3 to tanstack-start example

--- vanilla/with-tanstack-start/vite.config.ts
+++ vanilla/with-tanstack-start/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite';
 import viteSolid from 'vite-plugin-solid';
+import {nitro} from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -12,6 +13,7 @@ export default defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
+		nitro({}),
     viteSolid({ ssr: true }),
   ],
 });
