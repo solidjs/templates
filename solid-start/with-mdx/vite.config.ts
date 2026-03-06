368f5564ff27c76552cef7fabb548b655adea108
Birk Skyum <birk.skyum@pm.me>
2026-03-07 00:44:00 +0100

    use nitro v2 for stability

--- solid-start/with-mdx/vite.config.ts
+++ solid-start/with-mdx/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { nitro } from "nitro/vite";
+import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 
 import { solidStart } from "@solidjs/start/config";
 import mdx from "@mdx-js/rollup";
