4489b2cb6c47e2ad26a00c5411982eef2aeced3e
dofi4ka <dofi4ka@yandex.ru>
2026-06-10 00:40:48 +0300

    feat: add and adopt daisyui

--- solid-start-v2/with-daisyui/src/app.tsx
+++ solid-start-v2/with-daisyui/src/app.tsx
@@ -1,13 +1,13 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
-import "./app.css";
+import { Router } from '@solidjs/router';
+import { FileRoutes } from '@solidjs/start/router';
+import { Suspense } from 'solid-js';
+import Nav from '~/components/Nav';
+import './app.css';
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Nav />
           <Suspense>{props.children}</Suspense>
--- solid-start-v2/with-daisyui/src/routes/about.tsx
+++ solid-start-v2/with-daisyui/src/routes/about.tsx
@@ -1,25 +1,32 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
+import { A } from '@solidjs/router';
+import Counter from '~/components/Counter';
 
 export default function About() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">About Page</h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <span>About Page</span>
-      </p>
+    <main class="hero">
+      <section class="hero-content flex-col text-center justify-center">
+        <h1 class="max-6-xs text-6xl text-accent font-thin uppercase my-16">
+          About Page
+        </h1>
+        <Counter />
+        <p class="mt-8">
+          Visit{' '}
+          <a
+            href="https://solidjs.com"
+            target="_blank"
+            class="link link-accent link-hover"
+          >
+            solidjs.com
+          </a>{' '}
+          to learn how to build Solid apps.
+        </p>
+        <div class="menu menu-horizontal gap-2">
+          <A href="/" class="link link-accent link-hover">
+            Home
+          </A>
+          <span>About Page</span>
+        </div>
+      </section>
     </main>
   );
 }
